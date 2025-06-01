a44b22af9bf6a4a23123cc573c6d7c7d88d97e32
BarcodeGen <barcodegenn@gmail.com>
2025-06-01 16:43:35 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 import subprocess
 import tempfile
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 PS_TEMPLATE = """
 %!PS
